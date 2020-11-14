c639c2e565809d102a4de8ee95d785c361a29525
Robert Rozas Navarro <rorozasn@microsoft.com>
2020-11-14 19:56:43 -0300

    Demo

---
+++ Demo/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+    # We recommend pinning to the specific version of the Azure Provider you're using
+    # since new versions are released frequently
+    version = "=2.20.0"
+    features {}
+}
+
+# Resource Creation
+resource "azurerm_resource_group" "azurerg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "azurestor" {
+  name                     = var.sa_name
+  resource_group_name      = azurerm_resource_group.azurerg.name
+  location                 = azurerm_resource_group.azurerg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "Development"
+  }
+}
+
+# Information Output
+output "resource-group-id" {
+  value       = azurerm_resource_group.azurerg.id
+  description = "Azure Resource Group Internal ID."
+}
+
+
+
---
+++ Demo/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "eastus2"
+}
+
+variable "resource_group_name" {
+  default = "hashitalks-rg"
+}
+
+variable "sa_name" {
+  default = "hashitalkstorage"
+}
